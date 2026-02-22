9f585984163037e6d48d87aaa6396d8d7a2169c9
Claw <claw@fmckl.se>
2026-02-22 09:12:39 +0100

    fix: use correct ic-sql-migrate Builder API in build.rs

--- server/build.rs
+++ server/build.rs
@@ -1,4 +1,7 @@
 fn main() {
     ic_asset_router::build::generate_routes();
-    ic_sql_migrate::build::generate_migrations("src/migrations").unwrap();
+    ic_sql_migrate::Builder::new()
+        .with_migrations_dir("src/migrations")
+        .build()
+        .unwrap();
 }
